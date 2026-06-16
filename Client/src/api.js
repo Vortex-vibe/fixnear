4f67cdea879e21586dd5c6fd8f826a63fdda399d
Ayush Kumar <Kumarayush30405@gmail.com>
2026-06-16 11:04:36 +0530

    Update frontend backend URL

--- Client/src/api.js
+++ Client/src/api.js
@@ -1,3 +1,3 @@
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API_BASE_URL = "https://fixnear-server.onrender.com";
 
-export default API_URL;
+export default API_BASE_URL;
